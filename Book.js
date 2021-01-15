dea1807ede6e8e3690e4a7b087f83343d422aa91
Serik <example@gmail.com>
2021-01-15 17:01:58 +0600

    first commit

---
+++ Book.js
@@ -0,0 +1,3 @@
+const isChecked = true,
+      isClose = true;
+console.log(isChecked && isClose);
---
+++ JS.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Сколько тебе лет", "");
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0] = prompt('Как у вас дела?');
+//answers[1] = prompt('Как ваше имя?');
+//answers[2] = prompt('Сколько вам лет?');
+//document.write(answers);
+
+const user = "Serik";
+
+alert(`Hi, ${user}`);
