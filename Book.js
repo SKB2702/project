be3f2b30d59ae06a74e1830e2651c322a6a127ee
Serik <serik.kurmashev@nu.edu.kz>
2021-01-17 18:02:49 +0600

    practice work 1

--- Book.js
+++ Book.js
@@ -1,3 +1,18 @@
-const isChecked = true,
-      isClose = true;
-console.log(isChecked && isClose);
+const numberOfFilms = +prompt('Сколько вы посмотрели фильмов?', ''),
+a = prompt('Один из последних просмотренных фильмов?', ''),
+b = prompt('Насколько оцените фильм', ''),
+c = prompt('Насколько оцените качество фильма', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: [],
+    quality: []
+};
+
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.quality[a] = c;
+
+console.log(personalMovieDB);
