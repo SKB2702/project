da576d178e3bc4b0664859835310e74c08126aaa
Serik <serik.kurmashev@nu.edu.kz>
2021-02-01 16:33:07 +0600

    practice work 2 finished

--- Book.js
+++ Book.js
@@ -10,7 +10,20 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
 b = prompt('Насколько оцените фильм', '');
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
 personalMovieDB.movies[a] = b;
-};
+console.log('done');
+} else {
+    console.log('error');
+    i--;
+}
 
+};
+if (personalMovieDB.count < 10) {
+    console.log('Мало фильмов');
+} else if (personalMovieDB.count > 30) {
+    console.log('Киноман');
+} else {
+    console.log('Classical viewer');
+}
 console.log(personalMovieDB);
